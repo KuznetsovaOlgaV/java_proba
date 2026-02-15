7fef71a069502425fcafdeb02733f561d5c86072
KuznetsovaOlgaV <KuznetsovaOlgaV@yandex.ru>
2026-02-15 21:29:39 +0300

    старт

---
+++ proba/src/main/java/Hello.java
@@ -0,0 +1,21 @@
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println((2+2)*2);
+//
+//        System.out.println("hello, "+"world");
+//        System.out.println("2 + 2 = " + 4);
+//        System.out.println("2 + 2 = " + 2 + 2);
+//        System.out.println("2 + 2 = " + (2 + 2));
+
+
+
+
+    }
+}
