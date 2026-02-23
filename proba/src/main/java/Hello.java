0db97deb7d6b521dd08bb873d3ae7335ebfcfd6d
KuznetsovaOlgaV <KuznetsovaOlgaV@yandex.ru>
2026-02-23 18:27:02 +0300

    0

--- proba/src/main/java/Hello.java
+++ proba/src/main/java/Hello.java
@@ -4,16 +4,16 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello, world!");
 
-        System.out.println(2+2);
-        System.out.println(2*2);
-        System.out.println(2-2);
-        System.out.println(2/2);
-        System.out.println((2+2)*2);
-
-        System.out.println("hello, "+"world");
-        System.out.println("2 + 2 = " + 4);
-        System.out.println("2 + 2 = " + 2 + 2);
-        System.out.println("2 + 2 = " + (2 + 2));
+//        System.out.println(2+2);
+//        System.out.println(2*2);
+//        System.out.println(2-2);
+//        System.out.println(2/2);
+//        System.out.println((2+2)*2);
+//
+//        System.out.println("hello, "+"world");
+//        System.out.println("2 + 2 = " + 4);
+//        System.out.println("2 + 2 = " + 2 + 2);
+//        System.out.println("2 + 2 = " + (2 + 2));
 
         var configFile = new File("build.gradle");
         System.out.println(configFile.getAbsolutePath());
